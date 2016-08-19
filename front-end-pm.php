abdfad125be38720a36b8a31e4c51e4b56ae7360
shamim2883 <shamim_biplob@yahoo.com>
2016-08-20 00:03:57 +0600

    Front_End_Pm class add
    Front_End_Pm class add and all data move inside

--- front-end-pm.php
+++ front-end-pm.php
@@ -11,26 +11,81 @@ License: GPLv2 or later
 */
 //DEFINE
 
-define('FEP_PLUGIN_VERSION', '4.3' );
-define('FEP_PLUGIN_DIR',plugin_dir_path( __FILE__ ) );
-define('FEP_PLUGIN_URL',plugins_url( '/', __FILE__ ) );
+class Front_End_Pm {
 
-global $wpdb;
-
-if ( !defined ('FEP_MESSAGES_TABLE' ) )
-define('FEP_MESSAGES_TABLE',$wpdb->prefix.'fep_messages');
-
-if ( !defined ('FEP_META_TABLE' ) )
-define('FEP_META_TABLE',$wpdb->prefix.'fep_meta');
-
-require_once( FEP_PLUGIN_DIR. 'functions.php');
-
+	private static $instance;
 	
+	private function __construct() {
 
-	
-	
+		$this->constants();
+		$this->includes();
+		$this->actions();
+		//$this->filters();
 
-	//ACTIVATE PLUGIN
-	register_activation_hook(__FILE__ , 'fep_plugin_activate');
+	}
 	
+	public static function init()
+        {
+            if(!self::$instance instanceof self) {
+                self::$instance = new self;
+            }
+            return self::$instance;
+        }
+	
+	private function constants()
+    	{
+			define('FEP_PLUGIN_VERSION', '4.3' );
+			define('FEP_PLUGIN_FILE',  __FILE__ );
+			define('FEP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+			define('FEP_PLUGIN_URL', plugins_url( '/', __FILE__ ) );
+			
+			global $wpdb;
+			
+			if ( !defined ('FEP_MESSAGES_TABLE' ) )
+			define('FEP_MESSAGES_TABLE',$wpdb->prefix.'fep_messages');
+			
+			if ( !defined ('FEP_META_TABLE' ) )
+			define('FEP_META_TABLE',$wpdb->prefix.'fep_meta');
+    	}
+	
+	private function includes()
+    	{
+			require_once( FEP_PLUGIN_DIR. 'functions.php');
+
+			if( file_exists( FEP_PLUGIN_DIR. 'pro/pro-features.php' ) ) {
+				require_once( FEP_PLUGIN_DIR. 'pro/pro-features.php');
+			}
+    	}
+	
+	private function actions()
+    	{
+			register_activation_hook(__FILE__ , array($this, 'fep_plugin_activate' ) );
+			register_deactivation_hook(__FILE__ , array($this, 'fep_plugin_deactivate' ) );
+    	}
+	
+	function fep_plugin_activate(){
+
+		global $wpdb;
+		
+			$roles = array_keys( get_editable_roles() );
+			$id = $wpdb->get_var("SELECT ID FROM {$wpdb->posts} WHERE post_content LIKE '%[front-end-pm]%' AND post_status = 'publish' AND post_type = 'page' LIMIT 1");
+			
+			$options = array();
+			
+			$options['userrole_access'] = $roles;
+			$options['userrole_new_message'] = $roles;
+			$options['userrole_reply'] = $roles;
+			$options['plugin_version'] = FEP_PLUGIN_VERSION;
+			$options['page_id'] = $id;
+			
+			update_option( 'FEP_admin_options', wp_parse_args( get_option('FEP_admin_options'), $options) );
+			
+			fep_add_caps_to_roles();
+	
+	}
+	
+} //END Class
+
+Front_End_Pm::init();
+
 	
