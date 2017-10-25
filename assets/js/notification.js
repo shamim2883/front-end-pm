ffa5b1c93ca13cce25b94ed0a1ce21f53c3d12f1
Shamim Hasan <shamim_biplob@yahoo.com>
2017-10-25 22:47:19 +0600

    new js function fep_notification_ajax_call added

--- assets/js/notification.js
+++ assets/js/notification.js
@@ -1,36 +1,38 @@
 jQuery(document).ready(function(){
-var data = {
+
+	function fep_notification_ajax_call(){
+		var data = {
 			action: 'fep_notification_ajax',
 			token: fep_notification_script.nonce
-			};
-var fep_ajax_call = function(){
-	jQuery.post(fep_notification_script.ajaxurl, data, function(response) {
-		
-		jQuery('.fep_new_message_count').html(response['message_unread_count_i18n']);
-		if ( response['message_unread_count'] ){ 
-			jQuery('.fep_new_message_count_hide_if_zero').show();
-		} else {
-			jQuery('.fep_new_message_count_hide_if_zero').hide();
-		}
-		
-		jQuery('.fep_new_announcement_count').html(response['announcement_unread_count_i18n']);
-		if ( response['announcement_unread_count'] ){ 
-			jQuery('.fep_new_announcement_count_hide_if_zero').show();
-		} else {
-			jQuery('.fep_new_announcement_count_hide_if_zero').hide();
-		}
-		
-		jQuery('.fep-notification').html(response['notification']);
-		if ( response['notification'] ){ 
-			jQuery('.fep-notification').show();
-		} else {
-			jQuery('.fep-notification').hide();
-		}
-		jQuery('.fep_new_message_count_text').html(response['message_unread_count_text']);
-		jQuery('.fep_total_message_count').html(response['message_total_count_i18n']);
-		jQuery('.fep_new_announcement_count_text').html(response['announcement_unread_count_text']);
-		
-	}, 'json');
-}
-setInterval(fep_ajax_call, parseInt(fep_notification_script.interval, 10) );
+		};
+					
+		jQuery.post(fep_notification_script.ajaxurl, data, function(response) {
+			
+			jQuery('.fep_new_message_count').html(response['message_unread_count_i18n']);
+			if ( response['message_unread_count'] ){ 
+				jQuery('.fep_new_message_count_hide_if_zero').show();
+			} else {
+				jQuery('.fep_new_message_count_hide_if_zero').hide();
+			}
+			
+			jQuery('.fep_new_announcement_count').html(response['announcement_unread_count_i18n']);
+			if ( response['announcement_unread_count'] ){ 
+				jQuery('.fep_new_announcement_count_hide_if_zero').show();
+			} else {
+				jQuery('.fep_new_announcement_count_hide_if_zero').hide();
+			}
+			
+			jQuery('.fep-notification').html(response['notification']);
+			if ( response['notification'] ){ 
+				jQuery('.fep-notification').show();
+			} else {
+				jQuery('.fep-notification').hide();
+			}
+			jQuery('.fep_new_message_count_text').html(response['message_unread_count_text']);
+			jQuery('.fep_total_message_count').html(response['message_total_count_i18n']);
+			jQuery('.fep_new_announcement_count_text').html(response['announcement_unread_count_text']);
+			
+		}, 'json');
+	}
+setInterval(fep_notification_ajax_call, parseInt(fep_notification_script.interval, 10) );
 });
