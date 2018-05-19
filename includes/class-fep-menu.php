20e2786dd15f7529a45d33d49a71f12ac3565256
Shamim Hasan <shamim_biplob@yahoo.com>
2018-05-19 17:45:42 +0600

    Coding Standards applied in class-fep-menu.php

--- includes/class-fep-menu.php
+++ includes/class-fep-menu.php
@@ -4,32 +4,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-class Fep_Menu
-  {
- 	private static $instance;
+class Fep_Menu {
+	private static $instance;
 	
-	public static function init()
-        {
-            if(!self::$instance instanceof self) {
-                self::$instance = new self;
-            }
-            return self::$instance;
-        }
+	public static function init() {
+		if ( ! self::$instance instanceof self ) {
+			self::$instance = new self;
+		}
+		return self::$instance;
+	}
 		
-    function actions_filters()
-    	{
-			add_action ('fep_menu_button', array($this, 'menu'));
-    	}
+	function actions_filters(){
+			add_action( 'fep_menu_button', array( $this, 'menu' ) );
+		}
 
 	function menu(){
 		$menu = '';
 
 		foreach( $this->get_menu() as $menu_array ) {
 			$class = $menu_array['class'];
-			if ( isset($_GET['fepaction']) && $_GET['fepaction'] == $menu_array['action'])
+			if ( isset( $_GET['fepaction'] ) && $_GET['fepaction'] == $menu_array['action'] )
 			$class = $menu_array['active-class'];
 
-			$menu .= sprintf('<a id="%1$s" class="%2$s" href="%3$s">%4$s</a>',
+			$menu .= sprintf( '<a id="%1$s" class="%2$s" href="%3$s">%4$s</a>',
 				$menu_array['id'],
 				fep_sanitize_html_class( $class ),
 				fep_query_url( $menu_array['action'] ),
@@ -42,49 +39,44 @@ class Fep_Menu
 	public function get_menu()
 	{
 		$menu = array(
-				'newmessage'	=> array(
-					'title'			=> __('New Message', 'front-end-pm'),
-					'action'			=> 'newmessage',
-					'priority'			=> 5
-					),
-				'message_box'	=> array(
-					'title'			=> sprintf(__('Message Box%s', 'front-end-pm'), fep_get_new_message_button() ),
-					'action'			=> 'messagebox',
-					'priority'			=> 10
-					),
-				'settings'	=> array(
-					'title'			=> __('Settings', 'front-end-pm'),
-					'action'			=> 'settings',
-					'priority'			=> 15
-					),
-							
-				);
+			'newmessage'	=> array(
+				'title'			=> __( 'New Message', 'front-end-pm' ),
+				'action'		=> 'newmessage',
+				'priority'		=> 5,
+				),
+			'message_box'	=> array(
+				'title'			=> sprintf(__( 'Message Box%s', 'front-end-pm' ), fep_get_new_message_button() ),
+				'action'		=> 'messagebox',
+				'priority'		=> 10,
+				),
+			'settings'	=> array(
+				'title'			=> __( 'Settings', 'front-end-pm' ),
+				'action'		=> 'settings',
+				'priority'		=> 15,
+				),
+		);
 		if( ! fep_current_user_can( 'send_new_message' ) ) {
-			unset($menu['newmessage']);
+			unset( $menu['newmessage'] );
 		}
 							
-		$menu = apply_filters('fep_menu_buttons', $menu );
+		$menu = apply_filters( 'fep_menu_buttons', $menu );
 						
-				foreach ( $menu as $key => $tab )
-					{
-				
-						$defaults = array(
-								'title'			=> '',
-								'action'		=> $key,
-								'id'			=> 'fep-menu-' . $key,
-								'class'			=> 'fep-button',
-								'active-class'	=> 'fep-button-active',
-								'priority'		=> 20
-							);
-					$menu[$key] = wp_parse_args( $tab, $defaults);
-			
-				}
-			uasort( $menu, 'fep_sort_by_priority' );
-							
+		foreach ( $menu as $key => $tab ){
+			$defaults = array(
+				'title'			=> '',
+				'action'		=> $key,
+				'id'			=> 'fep-menu-' . $key,
+				'class'			=> 'fep-button',
+				'active-class'	=> 'fep-button-active',
+				'priority'		=> 20
+			);
+			$menu[ $key ] = wp_parse_args( $tab, $defaults);
+		}
+		uasort( $menu, 'fep_sort_by_priority' );				
 		return $menu;
 	}
 	
  } //END CLASS
 
-add_action('init', array(Fep_Menu::init(), 'actions_filters'));
+add_action( 'init', array( Fep_Menu::init(), 'actions_filters' ) );
 
