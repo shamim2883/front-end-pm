09eb19c4cb07c327515c14a0aac47da3e4adc368
Vlăduț Ilie <vladilie94@gmail.com>
2018-05-24 16:43:26 +0300

    WordPress JavaScript Coding Standards applied

--- assets/js/replies-show-hide.js
+++ assets/js/replies-show-hide.js
@@ -1,12 +1,11 @@
-jQuery(document).ready(function(){
-		jQuery(".fep-hide-if-js").hide();
-		jQuery(".fep-message-title").click(function () {
-			//open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
-			jQuery(this).next('.fep-message-content').slideToggle(500);
-		});
-		jQuery(".fep-message-toggle-all").click(function () {
-			//open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
-			jQuery('.fep-message-content').slideToggle(500);
-		});
-
+jQuery( document ).ready( function() {
+	jQuery( '.fep-hide-if-js' ).hide();
+	jQuery( '.fep-message-title' ).click( function () {
+		//open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+		jQuery( this ).next( '.fep-message-content' ).slideToggle( 500 );
+	});
+	jQuery( '.fep-message-toggle-all' ).click( function () {
+	//open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+	jQuery( '.fep-message-content' ).slideToggle( 500 );
+	});
 });
