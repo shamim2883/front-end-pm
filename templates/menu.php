3e0e7294b07a3cc488c859fac3c9cfc94d6b0bfb
Vlăduț Ilie <vladilie94@gmail.com>
2018-05-24 19:10:02 +0300

    WordPress PHP Coding Standards applied

--- templates/menu.php
+++ templates/menu.php
@@ -1,18 +1,10 @@
 <?php
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
-
 ?>
-
-	  
 <div id="fep-menu">
-
-	<?php do_action('fep_menu_button'); ?>
-
+	<?php do_action( 'fep_menu_button' ); ?>
 </div><!--#fep-menu -->
 <div id="fep-content">
-	  
-	<?php do_action('fep_display_before_content'); ?>
-
+<?php do_action( 'fep_display_before_content' ); ?>
