eff39817c7f07dae701e58a005ef02c401090b15
Shamim Hasan <shamim_biplob@yahoo.com>
2018-08-09 12:32:52 +0600

    placeholder not replaced by count in header

--- templates/header.php
+++ templates/header.php
@@ -21,12 +21,12 @@ if ( $max_total && ( ( $max_total * 90 ) / 100 ) <= $total_count ) {
 				<div><?php esc_html_e( 'You have', 'front-end-pm' ); ?>
 					<span class="fep_unread_message_count_text"><?php printf( _n( '%s message', '%s messages', $unread_count, 'front-end-pm' ), number_format_i18n( $unread_count ) ); ?></span>
 					<?php esc_html_e( 'and', 'front-end-pm' ); ?>
-					<span class="fep_unread_announcement_count_text"><?php echo esc_html( _n( '%s announcement', '%s announcements', $unread_ann_count, 'front-end-pm' ), number_format_i18n( $unread_ann_count ) ); ?></span>
+					<span class="fep_unread_announcement_count_text"><?php echo esc_html( sprintf( _n( '%s announcement', '%s announcements', $unread_ann_count, 'front-end-pm' ), number_format_i18n( $unread_ann_count ) ) ); ?></span>
 					<?php esc_html_e( 'unread', 'front-end-pm' ); ?>
 				</div>
 				<div class="<?php echo $box_class; ?>"><?php 
 					esc_html_e( 'Message box size:', 'front-end-pm' );
-					printf( __( '%1$s of %2$s', 'front-end-pm' ), '<span class="fep_total_message_count">' . number_format_i18n( $total_count ) . '</span>', $max_text ); ?>
+					echo strip_tags( sprintf( __( '%1$s of %2$s', 'front-end-pm' ), '<span class="fep_total_message_count">' . number_format_i18n( $total_count ) . '</span>', $max_text ), '<span>' ); ?>
 				</div>
 			</div>
 			<?php do_action( 'fep_header_note', $user_ID ); ?>
