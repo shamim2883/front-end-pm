7bbea1d1bb6e014a2612bf152e068d7f10b1ba5b
Shamim Hasan <shamim_biplob@yahoo.com>
2018-09-30 22:39:04 +0600

    menu now collapse for small screen device

--- templates/menu.php
+++ templates/menu.php
@@ -3,6 +3,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 ?>
+
+<button id="fep-menu-toggle-button" class="fep-button"><?php esc_html_e( 'Message Menu', 'front-end-pm' ); ?></button>
+<script type="text/javascript">
+document.getElementById('fep-menu-toggle-button').onclick = function() {
+	this.classList.toggle("fep-menu-toggle-expanded");
+	var fep_menu = document.getElementById("fep-menu");
+	if (fep_menu.style.display === "block") {
+		fep_menu.style.display = "none";
+	} else {
+		fep_menu.style.display = "block";
+	}
+};
+</script>
+
 <div id="fep-menu">
 	<?php do_action( 'fep_menu_button' ); ?>
 </div><!--#fep-menu -->
