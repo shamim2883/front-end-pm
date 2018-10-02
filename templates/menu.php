2ccbc71996efe8e6b617469523a8ca8caf81cd77
Shamim Hasan <shamim_biplob@yahoo.com>
2018-10-02 15:44:40 +0600

    Menu for small device was not showing when device width increase

--- templates/menu.php
+++ templates/menu.php
@@ -7,17 +7,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 <button id="fep-menu-toggle-button" class="fep-button"><?php esc_html_e( 'Message Menu', 'front-end-pm' ); ?></button>
 <script type="text/javascript">
 document.getElementById('fep-menu-toggle-button').onclick = function() {
-	this.classList.toggle("fep-menu-toggle-expanded");
-	var fep_menu = document.getElementById("fep-menu");
-	if (fep_menu.style.display === "block") {
-		fep_menu.style.display = "none";
-	} else {
-		fep_menu.style.display = "block";
-	}
+	this.classList.toggle('fep-menu-toggle-expanded');
+	document.getElementById("fep-menu").classList.toggle('fep-menu-toggle');
 };
 </script>
 
-<div id="fep-menu">
+<div id="fep-menu" class="fep-menu-toggle">
 	<?php do_action( 'fep_menu_button' ); ?>
 </div><!--#fep-menu -->
 <div id="fep-content">
