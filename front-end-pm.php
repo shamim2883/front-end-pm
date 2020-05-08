87e7679a695b4b615ec3ba6fe9507e996a2e5bdd
Shamim Hasan <shamim_biplob@yahoo.com>
2020-05-08 18:10:10 +0600

    Freemius added

--- front-end-pm.php
+++ front-end-pm.php
@@ -22,8 +22,7 @@ class Front_End_Pm {
 	private function __construct() {
 		$this->constants();
 		$this->includes();
-		$this->actions();
-		//$this->filters();
+		$this->hooks();
 	}
 
 	public static function init() {
@@ -60,15 +59,56 @@ class Front_End_Pm {
 		require_once( FEP_PLUGIN_DIR . 'default-hooks.php' );
 	}
 
-	function actions() {
-		register_activation_hook( __FILE__ , array( $this, 'fep_plugin_activate' ) );
-		register_deactivation_hook( __FILE__ , array( $this, 'fep_plugin_deactivate' ) );
+	function hooks() {
+		//cleanup after uninstall
+		fep_fs()->add_action('after_uninstall', 'fep_fs_uninstall_cleanup');
+		//Support fourm link in admin dashboard sidebar
+		fep_fs()->add_filter( 'support_forum_url', 'fep_fs_support_forum_url' );
 	}
 
-	function fep_plugin_activate() {
-	}
-
-	function fep_plugin_deactivate() {
-	}
 } //END Class
-Front_End_Pm::init();
+//Front_End_Pm::init();
+
+if ( function_exists( 'fep_fs' ) ) {
+	fep_fs()->set_basename( false, __FILE__ );
+} else {
+	// DO NOT REMOVE THIS IF, IT IS ESSENTIAL FOR THE `function_exists` CALL ABOVE TO PROPERLY WORK.
+	if ( ! function_exists( 'fep_fs' ) ) {
+		// Create a helper function for easy SDK access.
+		function fep_fs() {
+			global $fep_fs;
+	
+			if ( ! isset( $fep_fs ) ) {
+				// Include Freemius SDK.
+				require_once dirname(__FILE__) . '/freemius/start.php';
+	
+				$fep_fs = fs_dynamic_init( array(
+					'id'                  => '5809',
+					'slug'                => 'front-end-pm',
+					'premium_slug'        => 'front-end-pm-pro',
+					'type'                => 'plugin',
+					'public_key'          => 'pk_c7329ca7019f17b830c22b8f3a729',
+					'is_premium'          => false,
+					'premium_suffix'      => 'PRO',
+					// If your plugin is a serviceware, set this option to false.
+					'has_premium_version' => true,
+					'has_addons'          => false,
+					'has_paid_plans'      => true,
+					'menu'                => array(
+						'slug'           => 'fep-all-messages',
+					),
+				) );
+			}
+	
+			return $fep_fs;
+		}
+	
+		// Init Freemius.
+		fep_fs();
+		// Signal that SDK was initiated.
+		do_action( 'fep_fs_loaded' );
+	}
+
+	// ... Your plugin's main file logic ...
+	Front_End_Pm::init();
+}
