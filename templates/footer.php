b3429bfa2981f4502612167ee1a6742c1b857727
Shamim Hasan <shamim_biplob@yahoo.com>
2022-12-24 12:12:11 +0600

    Add space after <!--

--- templates/footer.php
+++ templates/footer.php
@@ -3,8 +3,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 ?>
-		</div><!--#fep-content -->
+		</div><!-- #fep-content -->
 	<div id="fep-footer">
 	<?php do_action( 'fep_footer_note' ); ?>
-	</div><!--#fep-footer -->
-</div><!--#fep-wrapper -->
+	</div><!-- #fep-footer -->
+</div><!-- #fep-wrapper -->
--- templates/menu.php
+++ templates/menu.php
@@ -14,6 +14,6 @@ document.getElementById('fep-menu-toggle-button').onclick = function() {
 
 <div id="fep-menu" class="fep-menu-toggle">
 	<?php do_action( 'fep_menu_button' ); ?>
-</div><!--#fep-menu -->
+</div><!-- #fep-menu -->
 <div id="fep-content">
 <?php do_action( 'fep_display_before_content' ); ?>
